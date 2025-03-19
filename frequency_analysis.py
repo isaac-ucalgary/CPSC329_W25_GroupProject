3c2c9255770fa139620e6f29f6aa353874b0a5ce
james.clark4@ucalgary.ca <james.clark4@ucalgary.ca>
2025-03-19 16:13:01 -0600

    Initial work on frequency analysis.

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,4 +1,14 @@
-# frequency analysis.
+# performs frequency analysis the string passed to it. we can wrap this with file i/o and other options later, if needed.
+# But, we can just call it with a string the user puts in a textbox on the web frontend
+# This will return ???
+def analyse_freq(string):
+    frequencies = {}
+    # Add and increment a letter in the dictionary. Will work for any recognized letter
+    for s in string:
+        if s in frequencies:
+            frequencies[s] += 1
+        else: 
+            frequencies.update({s : 1})
 
-def frequency_analysis():
-    return False
+
+analyse_freq("aaaa")
