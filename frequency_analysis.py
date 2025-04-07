0dea3638793557499f3ba5acc0d561b436ea7fdc
james.clark4@ucalgary.ca <james.clark4@ucalgary.ca>
2025-04-06 20:17:05 -0600

    no changes really

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,4 +1,5 @@
-# performs frequency analysis the string passed to it. we can wrap this with file i/o and other options later, if needed.
+# performs frequency analysis the string passed to it.
+# we can wrap this with file i/o and other options later, if needed.
 # But, we can just call it with a string the user puts in a textbox on the web frontend
 # This will return ???
 def analyse_freq(string):
@@ -7,10 +8,12 @@ def analyse_freq(string):
     for s in string:
         if s in frequencies:
             frequencies[s] += 1
-        else: 
-            frequencies.update({s : 1})
+        else:
+            frequencies.update({s: 1})
+    # Format dict into a string we can return to the website.
     for f in frequencies:
         print(f"{f} : {frequencies[f]}")
 
 
-analyse_freq("The quick brown fox jumps over the lazy dog.")
+# Test function.
+analyse_freq(input("Enter the string to frequency analyse: "))
