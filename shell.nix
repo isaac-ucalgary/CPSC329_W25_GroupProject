6268b7def910f96c3916ebe5906c91cd131e549a
isaac-ucalgary <isaac.shiellsthomas@ucalgary.ca>
2025-04-08 22:06:26 -0600

    Created nix shell

---
+++ shell.nix
@@ -0,0 +1,24 @@
+# shell.nix
+let
+  pkgs = import <nixpkgs> {};
+
+  python = pkgs.python314Full.override {
+    self = python;
+    # packageOverrides = pyfinal: pyprev: {
+    #   # mysql-connector = pyfinal.callPackage package-mysql-connector { };
+    # };
+  };
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      (python.withPackages (
+        python-pkgs:
+          with python-pkgs; [
+            # select Python packages here
+            # matplotlib
+          ]
+      ))
+    ];
+
+    shellHook = '''';
+  }
